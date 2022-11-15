87eb3f8d8a34b9045afb5c8e78802041ae38089c
cutthroat78 <irisharmycat2@gmail.com>
2022-11-15 09:46:16 +0000

    median program in class

---
+++ My Python Learning/median.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+unsorted_list = [] # defines a empty list
+while True: # make a infinite loop
+    number = input("Input a number for the list, type q to quit inputting numbers\n") # ask the user for a number and set that number to the variable named variable
+    if number == "q": # if variable named number is q
+        break # break the infinite loop
+    else: # if it is not q
+        my_list.append(int(number)) # convert the string variable named number to an integer and append it to the list called my_list
+
+my_list = list.sort(unsorted_list)
+
+if (len(my_list) % 2) == 0:
+    middle = len(my_list)/2
+    middle1 = my_list[int(middle)]
+    middle2 = my_list[int(middle)+1]
+    print(int(middle1) + int(middle2) / 2)
+else:
+     print("To be done")
